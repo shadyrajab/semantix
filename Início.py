114e0cc33ded2593b1289af7bac4407272923670
shady <shadyrajaab@gmail.com>
2024-05-21 15:39:12 -0300

    feat: adicionar paginação

--- Início.py
+++ Início.py
@@ -5,10 +5,11 @@ from components.home.summoner import search_summoner_bar
 st.set_page_config(
     page_title="Lol Stats",
     page_icon="https://i.imgur.com/7ZKdi2h.jpeg",
-    layout="centered",
+    layout="wide",
 )
 
 with open("styles/main.css", "r") as css:
     st.markdown(f"<style>{css.read()}</style>", unsafe_allow_html=True)
 
-search_summoner_bar()
+with st.container(border=True):
+    search_summoner_bar()
